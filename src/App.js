771c7f049bbb1c287ae0471f1ae1c3108e4e2dfd
Osva Fernandez <osvanaranjo96@gmail.com>
2022-09-24 15:44:51 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import Inicio from "./components/Inicio";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <hr className="m-0 mt-3 py-3" />
+      <Inicio />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Inicio.jsx
@@ -0,0 +1,11 @@
+const Inicio = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] w-full mx-auto flex flex-col justify-center">
+        <h3 className="mx-auto">Evento de Harley-Davidson en Cuba</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Inicio;
