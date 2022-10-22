992981c86cd9260ad30636bfb4297b2026c903be
ArieldRio <arieldelrioviamonte@gmail.com>
2022-10-22 03:12:37 -0400

    Format files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar';
+import Inicio from './components/Inicio';
+import ProximoEvento from './components/ProximoEvento';
+import Galeria from './components/Galeria';
+import Contactos from './components/Contactos';
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<hr className=" border-orange-600 m-0 mt-3 py-1" />
+			<Inicio />
+			<hr className="border-3 border-orange-600 m-0 mt-3 py-3" />
+			<ProximoEvento />
+			<hr className="border-3 border-orange-600 m-0 mt-3 py-3" />
+			<Galeria />
+			<hr className="border-3 border-orange-600 m-0 mt-3 py-3" />
+			<Contactos />
+		</>
+	);
+}
+
+export default App;
--- src/components/Contactos.jsx
+++ src/components/Contactos.jsx
@@ -1,17 +1,17 @@
-import p01 from "./imgs/p01.webp";
-import { BsFacebook, BsTwitter, BsGithub, BsWhatsapp } from "react-icons/bs";
+import { BsFacebook, BsTwitter, BsGithub, BsWhatsapp } from 'react-icons/bs';
+import p01 from './imgs/p01.webp';
 
-const Contactos = () => {
-  return (
-    <footer id="footer" className="p-4 bg-white sm:p-6 dark:bg-black">
-      <div className="md:flex md:justify-between">
-        <div className=" mb-6 md:mb-0 flex flex-row">
-          <img src={p01} className="mr-3 h-10 rounded" alt="H-D Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap text-orange-600">
-            Harlistas Cubanos
-          </span>
-        </div>
-        {/* <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+function Contactos() {
+	return (
+		<footer id="footer" className="p-4 bg-white sm:p-6 dark:bg-black">
+			<div className="md:flex md:justify-between">
+				<div className=" mb-6 md:mb-0 flex flex-row">
+					<img src={p01} className="mr-3 h-10 rounded" alt="H-D Logo" />
+					<span className="self-center text-2xl font-semibold whitespace-nowrap text-orange-600">
+						Harlistas Cubanos
+					</span>
+				</div>
+				{/* <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
           <div>
             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
               Resources
@@ -64,47 +64,42 @@ const Contactos = () => {
             </ul>
           </div>
         </div> */}
-      </div>
-      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <div className="sm:flex sm:items-center sm:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2022
-        </span>
-        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-          <a
-            href="https://wa.me/+5358117628"
-            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-          >
-            <BsWhatsapp />
-            <span className="sr-only">WhatsApp contact</span>
-          </a>
-          <a
-            href="https://m.facebook.com/profile.php?id=100032787713039&ref=py_c&_rdr"
-            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-          >
-            <BsFacebook />
-            <span className="sr-only">Facebook page</span>
-          </a>
+			</div>
+			<hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+			<div className="sm:flex sm:items-center sm:justify-between">
+				<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022</span>
+				<div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+					<a
+						href="https://wa.me/+5358117628"
+						className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+					>
+						<BsWhatsapp />
+						<span className="sr-only">WhatsApp contact</span>
+					</a>
+					<a
+						href="https://m.facebook.com/profile.php?id=100032787713039&ref=py_c&_rdr"
+						className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+					>
+						<BsFacebook />
+						<span className="sr-only">Facebook page</span>
+					</a>
 
-          <a
-            href="#"
-            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-          >
-            <BsTwitter />
+					<a href="/#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+						<BsTwitter />
 
-            <span className="sr-only">Twitter page</span>
-          </a>
-          <a
-            href="https://github.com/osvafernandez/harlistas-cubanos"
-            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-          >
-            <BsGithub />
-            <span className="sr-only">GitHub account</span>
-          </a>
-        </div>
-      </div>
-    </footer>
-  );
-};
+						<span className="sr-only">Twitter page</span>
+					</a>
+					<a
+						href="https://github.com/osvafernandez/harlistas-cubanos"
+						className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+					>
+						<BsGithub />
+						<span className="sr-only">GitHub account</span>
+					</a>
+				</div>
+			</div>
+		</footer>
+	);
+}
 
 export default Contactos;
