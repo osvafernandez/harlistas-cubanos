6a3392a2da5efced4f815e5aee893cc79f9fa45a
osvaFernandez <osvanaranjo96@gmail.com>
2023-08-10 15:50:26 -0400

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { Navbar, Inicio, Contactos, Galeria, ProximoEvento } from './components';
+import { Navbar, Contactos, Galeria, ProximoEvento } from './components';
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<hr className=" border-orange-600 m-0 mt-3 py-1" />
-			<Inicio />
+			{/* <Inicio /> */}
 			{/* <hr className="border-3 border-orange-600 m-0 mt-3 py-3" /> */}
 			<ProximoEvento />
 			<hr className="border-3 border-orange-600 m-0 mt-3 py-3" />
