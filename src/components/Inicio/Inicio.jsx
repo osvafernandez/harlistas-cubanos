278bef7b1952a639b3f94d18ea4aa6ebf72d9a84
osvaFernandez <osvanaranjo96@gmail.com>
2023-08-10 14:11:52 -0400

    home component updated

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,28 +1,17 @@
-import logo from '../../assets/images/logo.webp';
+// import logo from '../../assets/images/logo.webp';
+import AiPhoto from '../../assets/IA Generated photos/fotor-ai-20230810115147.jpg'
 
 function Inicio() {
 	return (
 		<div id="inicio" className="text-white">
 			<div className="flex flex-col justify-center w-full mx-auto max-w-35">
-				<div className="flex flex-col items-center justify-center mx-5 my-3">
-					<img className="rounded-lg max w-32 h-auto my-4" src={logo} alt="" />
+				<div className='my-3 flex'>
+					<p className='w-full md:w-1/2 ml-10'>
+					Una vez más, nos preparamos para celebrar el 13º evento anual de Harley-Davidson. Este evento ha crecido año tras año, convirtiéndose en un hito importante en el calendario para los entusiastas de las motocicletas de todo el país y más allá
+					</p>
+					<img className='rounded-lg w-full md:w-1/2 h-auto' src={AiPhoto} alt="" />
 				</div>
 				<hr className="border-orange-600 m-0" />
-				{/* <div className="my-3">
-					<p className="flex text-center p-3 mx-4 font-sans text-lg font-semibold">
-						Cuba a pesar de ser una isla, la mayor del mar de las Antillas y separada de los grandes
-						continentes tampoco ha quedado exenta de ser parte de la historia de la Harley-Davidson, la
-						afamada y lujosa motocicleta que nació un día allá por el año de 1903 en Milwaukee, Ciudad de
-						Norteamérica, y que es actualmente la motocicleta más reconocida a nivel mundial. La carencia de
-						partes y piezas para estas motocicletas provocadas por el embargo comercial al que ha estado
-						sometido el país por más de 50 años ha hecho que la historia de los “Harlistas Cubanos” sea tan
-						apasionante y mística como lo es la misma historia de la Harley-Davidson. Con estas paginas
-						queremos dejar plasmada la semblanza de algunos de los más emblemáticos personajes
-						contemporáneos a nuestra época y que han traspasado la frontera del solo hecho de poseer una de
-						estas motocicletas, para convertirse a sí mismos en lo que son hoy los “Harlistas Cubanos”. A
-						todos ellos, sus familiares y amigos va dedicada esta obra.
-					</p>
-				</div> */}
 			</div>
 		</div>
 	);
